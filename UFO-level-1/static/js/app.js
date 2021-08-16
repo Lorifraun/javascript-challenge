6366ec7e39733fa442be35ae3e3b9e07d9653e11
lorifraun <lorifraun@live.com>
2021-08-16 11:44:02 -0600

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody=d3.select("tbody");
+// loop through data and console log each record
+data. forEach(function(sightingReport){
+    console.log(sightingReport)
+});
+console.log(tableData.length)
+  
+  buildTable(tableData);
+
+function filterData(){
+  var newData =data;
+  var newDate =d3.select("#datetime").property("value")
+  if (newDate){
+    newData=newData.filter(obj =>obj.datetime===newDate)
+  }
+  buildTable(newData);
+}
+function buildTable (data){
+  tbody.html("")
+  data.forEach((sightingReport) => {
+    var row = tbody.append("tr");
+    Object.entries(sightingReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+  d3.select("#filter-btn").on("click", filterData);
